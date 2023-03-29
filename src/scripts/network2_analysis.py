d6b11be254088dbf3c245591841030e179e24a8a
MaddaGh <92437363+MaddaGh@users.noreply.github.com>
2023-03-29 23:45:37 +0200

    retrieveNationalities

---
+++ src/scripts/network2_analysis.py
@@ -0,0 +1,12 @@
+# CENTRALITY MEASURES
+    # Degree centrality
+    # Eigenvector centrality
+    # Closeness centrality ( most well connected agents)
+
+    # what are the movies in which our most important (influential and well connected) agents of Network 2 participated and how that is related to the influence ranking of the movie
+    # components - clustering coefficient - local clustering coefficient - redundancy (e the connectedness of the network as well as individuate structural holes, hence those nodes, whose missing links give it more control over diffusion of knowledge and information between neighbors)
+
+# ====== code for retrieving a person nationality from wikidata ======
+
+
+# ==================== #
