a68f713a59302b4489de57045bed2a6326019f62
Chloe Papadopoulou <92371498+chloeppd@users.noreply.github.com>
2024-07-26 11:54:00 +0200

    cleanup

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -16,8 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 project for the course of Network Analysis - 2022/2023
-Realized by.
-	Andrea D'Arpa
+Realized by
 	Maddalena Ghiotto		
 	Chloe Papadopoulou
 
